edfcd9d6c0a6f4882de7bae46f0f2c5521bcbde2
yaacov <yaacov.levy@epitech.eu>
2024-12-07 04:52:51 +0100

    [ADD]: Adding this line in validator : console.log('Validation errors:', errors.array()); because I begin to see some shit like this in Render : POST /api/auth/register - Body: {   username: 'y_levy@hotmail.fr',   email: 'checkjhskfj236677',   password: 'checkchecj' } WELL PLAYED FRONTEND OF SUNDAY

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -14,6 +14,7 @@ exports.validateLogin = [
 exports.validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    console.log('Validation errors:', errors.array());
     return res.status(400).json({ errors: errors.array() });
   }
   next();
