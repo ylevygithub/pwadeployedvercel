a84ba9de43d64b14573f7554555d68b1664dc020
yaacov <yaacov.levy@epitech.eu>
2024-12-07 04:49:10 +0100

    [ADD]: Le front envoie les mauvaises données depuis le début, front fait a la va vite, il est presque 5h du mat jpp

--- src/services/api.js
+++ src/services/api.js
@@ -15,7 +15,7 @@ export const api = {
     const response = await fetch(`${API_URL}/auth/register`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, email, password })
+      body: JSON.stringify({ email, password, username })
     });
     return response.json();
   },
